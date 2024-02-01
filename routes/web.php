e0622e7692be0ad2bfb6e3bc4d6ff3e9cc517de5
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2024-02-01 11:49:06 -0500

    Update
    Update

--- routes/web.php
+++ routes/web.php
@@ -13,75 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
-Route::get("/tasks", function () use ($tasks) {
-    return view('tasks.home', ['tasks' => $tasks]);
+Route::get("/tasks", function () {
+    return view('tasks.home', ['tasks' => \App\Models\Task::latest()->get()]);
 })->name('tasks.home');
 
 Route::get("/", function () {
     return redirect()->route('tasks.home');
 });
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
-
-    if (!$tasks) {
-        return abort(404);
-    }
-
-    return view('tasks.show', ['task' => $task]);
+Route::get('/tasks/{id}', function ($id) {
+    return view('tasks.show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::fallback(function () {
