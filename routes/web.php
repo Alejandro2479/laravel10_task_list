9526d9dbd6b4dd2d53dd673b84ad1b34fc2fde90
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2024-02-05 15:40:07 -0500

    Update
    Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,7 @@ use Illuminate\Http\Request;
 */
 
 Route::get("/tasks", function () {
-    return view('tasks.home', ['tasks' => \App\Models\Task::latest()->get()]);
+    return view('tasks.home', ['tasks' => Task::latest()->get()]);
 })->name('tasks.home');
 
 Route::get("/", function () {
@@ -25,11 +26,23 @@ Route::get("/", function () {
 Route::view('/tasks/create', 'tasks.create');
 
 Route::get('/tasks/{id}', function ($id) {
-    return view('tasks.show', ['task' => \App\Models\Task::findOrFail($id)]);
+    return view('tasks.show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('taks.store');
 
 Route::fallback(function () {
