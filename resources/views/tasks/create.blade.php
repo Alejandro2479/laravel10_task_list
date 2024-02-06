ccbdb9e656cd6a8f2d74e037bb7f98fc0bc4f490
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2024-02-06 11:18:01 -0500

    Update
    Update

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -15,7 +15,7 @@
         @csrf
         <div>
             <label for="title">Title</label>
-            <input text="text" name="title" id="title">
+            <input text="text" name="title" id="title" value="{{ old('title') }}">
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -23,7 +23,7 @@
 
         <div>
             <label for="description">Description</label>
-            <input text="description" name="description" id="description" rows="5">
+            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -31,7 +31,7 @@
 
         <div>
             <label for="long_description">Long Description</label>
-            <input text="long_description" name="long_description" id="long_description" rows="10">
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
