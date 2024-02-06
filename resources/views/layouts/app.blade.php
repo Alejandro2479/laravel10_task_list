89dc286cce573d28675667c33f28f463984a4ebe
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2024-02-06 08:52:28 -0500

    Update
    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tasks List</title>
+    @yield('styles')
 </head>
 <body>
     <h1>@yield('title')</h1>
 
     <div>
+        @if(session()->has('success'))
+            <div>
+                {{ session('success') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,22 +2,39 @@
 
 @section('title', 'Add Task')
 
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0, 8rem;
+        }
+    </style>
+
 @section('content')
     <form method="POST" action="{{ route('taks.store') }}">
         @csrf
         <div>
             <label for="title">Title</label>
             <input text="text" name="title" id="title">
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="description">Description</label>
             <input text="description" name="description" id="description" rows="5">
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="long_description">Long Description</label>
             <input text="long_description" name="long_description" id="long_description" rows="10">
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
