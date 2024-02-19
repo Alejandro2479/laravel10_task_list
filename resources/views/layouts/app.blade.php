78c9078f2c88d14287ae16af896a21bbad6e4660
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2024-02-18 21:18:57 -0500

    Update
    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,17 +16,33 @@
         .link {
             @apply font-medium text-gray-700 underline decoration-green-500
         }
+
+        label {
+            @apply block uppercase text-slate-700 mb-2
+        }
+
+        input, textarea {
+            @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+        }
+
+        .error {
+            @apply text-red-500 text-sm
+        }
+
+        .success {
+            @apply text-green-500 text-sm mb-4
+        }
     </style>
     {{-- blade-formatter-enable --}}
 
     @yield('styles')
 </head>
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
-    <h1 class="text-2xl mb-4">@yield('title')</h1>
+    <h1 class="text-2xl mb-4 font-medium">@yield('title')</h1>
 
     <div>
         @if(session()->has('success'))
-            <div>
+            <div class="success">
                 {{ session('success') }}
             </div>
         @endif
--- resources/views/tasks/form.blade.php
+++ resources/views/tasks/form.blade.php
@@ -1,39 +1,40 @@
 @extends('layouts.app')
 
-@section('title', isset($task) ? 'Edit Task' : 'Add Task')
+@section('title', isset($task) ? 'EDIT TASK' : 'ADD A TASK')
 
 @section('content')
-    <form action="{{ isset($task) ? route('tasks.edit', ['task' => $task->id]) : route('tasks.store') }}" method="POST">
+    <form action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}" method="POST">
+        
         @csrf
         @isset($task)
             @method('PUT')
         @endisset()
-        <div>
+        <div class="mb-4">
             <label for="title">Title</label>
-            <input text="text" name="title" id="title" value="{{ $task->title ?? old('title') }}">
+            <input text="text" name="title" id="title" @class(['border-red-500' => $errors->has('title')]) value="{{ $task->title ?? old('title') }}">
             @error('title')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
 
-        <div>
+        <div class="mb-4">
             <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5">{{ $task->description ?? old('description') }}</textarea>
+            <textarea name="description" id="description" rows="5" @class(['border-red-500' => $errors->has('description')])>{{ $task->description ?? old('description') }}</textarea>
             @error('description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
 
-        <div>
+        <div class="mb-4">
             <label for="long_description">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
+            <textarea name="long_description" id="long_description" rows="10" @class(['border-red-500' => $errors->has('long_description')])>{{ $task->long_description ?? old('long_description') }}</textarea>
             @error('long_description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
 
-        <div>
-            <button type="submit">
+        <div class="flex gap-2">
+            <button class="btn" type="submit">
                 @isset($task)
                     UPDATE TASK
                 @else
@@ -41,5 +42,9 @@
                 @endisset
             </button>
         </div>
+
+        <nav class="mt-4">
+            <a class="link" href="{{ route('tasks.home') }}">GO BACK</a>
+        </nav>
     </form>
 @endsection
